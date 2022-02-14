8a54e294b180650eda19e2819fe43701d1bd42c3
ffan <ffan@nvidia.com>
2022-02-14 22:02:45 +0800

    several new usage of pandas

--- test_pandas.py
+++ test_pandas.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 import pandas as pd
+import re
 import os
 
 
@@ -49,8 +50,52 @@ def test3(file1=r"d:\a.csv", file2=r"d:\b.csv"):
 
 def test4():
     df = pd.read_csv(r"nba.csv")
-    columns = df.colomuns
-    print(df.head(5))
+    columns = df.columns
+    # read each column
+    # col_by_name = df['Team']
+    #  try df.Team to read column Team
+    col_by_name = df.Name
+    col1 = df['Name'][1:5]
+    # read multiple columns
+    col2 = df[['Name', 'Team', 'Age']]
+    # read each row
+    rows = df.iloc[1]
+    rows = df.iloc[1:4]
+    # read a specific location (row, col)
+    element1 = df.iloc[1, 2]
+    element2 = df.iloc[1:4, 2:5]
+    print("df.iterrows()")
+    for index, row in element2.iterrows():
+        print(index, row)
+    # conditional dataframe or filtering data
+    element3 = df.loc[df['Age'] == 25]
+    # sort dataframe  sort1 = df.sort_values('Age', ascending=False)
+    # ascending by age and descending by team
+    sort1 = df.sort_values(['Age', 'Team'], ascending=[1, 0])
+    desc = df.describe()
+
+    # make changes to data
+    df['number+age'] = df['Number'] + df['Age']
+    df_1 = df.drop(columns=['number+age'])
+    df_1 = df[['Name', 'Age', 'Team']]
+    # save modified df to csv by df.to_csv('new_name.csv') or to excel df.to_excel
+    # df.to_csv('new_name.csv', index=False)
+    filter_df1 = df.loc[(df['Age'] == 25) & (df['Position'] == 'SF')]
+
+    # filtering data
+    # filter_df1.reset_index(drop=True) to remove old index
+    # filter_df1 = filter_df1.reset_index()
+    # filter_df2 = df.loc[(df['Age'] == 25) | (df['Salary'] > 100000)]
+    #  df.loc[~df['Name'].str.contains("ph", na=False)] here ~ meaning not
+    # filter_df3 = df.loc[~df['Name'].str.contains("ph", na=False)]
+    filter_df4 = df.loc[df['Name'].str.contains("ph|ev", na=False, regex=True, flags=re.I)]
+
+    # conditional changes
+    # change inplace
+    # df.loc[df['Team'] == 'Brooklyn Nets', 'Team'] = 'Atom nets'
+    # df.loc[df['Team'] == 'Brooklyn Nets', 'College'] = 'Asernal'
+    # df.loc[df['Weight'] > 210, ['College', 'Salary']] = ['AAAAA', '10020']
+    print(df.head(2))
 
 
 if __name__ == "__main__":
