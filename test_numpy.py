8b05fb1edb9221f057487c0d3f2d07170676341d
fancy fan <ffan@nvidia.com>
2022-02-24 17:10:44 +0800

    add more numpy cases

--- test_numpy.py
+++ test_numpy.py
@@ -1,4 +1,5 @@
 import numpy as np
+from setuptools import sic
 
 
 def demo1():
@@ -18,6 +19,33 @@ def demo1():
     # [startindex:endindex:stepsize]
 
     a[:, 1] = [10, 20]
+
+    # initializing different types of arrays
+    a1 = np.zeros((3, 4))
+    a2 = np.ones((3, 4, 5), dtype='int16')
+    a3 = np.full((2, 3), 99)
+    # np.full(a1.shape, 88)
+    a4 = np.full_like(a1, 88, dtype='int')
+    a5 = np.random.rand(5, 4, 3)
+    a6 = np.random.sample(a1.shape)
+    a7 = np.random.randint(0, 10, size=(3, 3))
+    a8 = np.identity(5)
+
+    # deep copy for arrays
+    b = a1.copy()
+
+    # op on array
+    c1 = a1 + 2
+    c2 = a5 ** 2
+    c3 = np.sin(a5)
+
+    # Linear algebra
+    d1 = np.random.randint(10, size=(2, 3))
+    d2 = np.random.randint(10, size=(3, 2))
+    d3 = np.matmul(d1, d2)
+    # np.linalg.det(d3)
+
+    # statistics
     print(a)
 
 
