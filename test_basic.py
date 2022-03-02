addd774f9c3430a5b70541d5bf1c271e1f59fc38
fancy fan <ffan@nvidia.com>
2022-03-02 17:25:53 +0800

    try cass: test_basic, test_requests

--- test_basic.py
+++ test_basic.py
@@ -8,8 +8,18 @@ def demo():
 
     # list all env variables
     for k, v in sorted(os.environ.items()):
-        print(k+':', v)
+        print(k + ':', v)
+
+
+def demo1():
+    cmd = "axel %s -o %s -p" % ('url_dw', 'f')
+    cmd1 = "axel {c1} -o {c2} -p".format(c1='aa', c2='dd')
+
+    cwd = os.getcwd()
+    os.chdir('/home')
+    os.chdir(cwd)
+    print(cmd1)
 
 
 if __name__ == '__main__':
-    demo()
+    demo1()
