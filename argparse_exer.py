810d5872dc811253babe98c1af253c3e8bd38c19
ffan <ffan@nvidia.com>
2022-05-29 20:27:43 +0800

    demo subprocess popen and run.

--- argparse_exer.py
+++ argparse_exer.py
@@ -1,13 +1,26 @@
 import argparse
+import os
+
+
+def save_env(env_name):
+    env = ''
+    with open(env_name, 'w') as fp:
+        for k in os.environ:
+           str_ = f'{k}={os.environ[k]}\n'
+           env += str_
+           # print(str_)
+        fp.writelines(env)
 
 
 def demo_argparse():
     parser = argparse.ArgumentParser(description='name')
     parser.add_argument('--family', type=str, help='first name')
     parser.add_argument('--name', type=str, dest='ffan_name', metavar='meta', help='last name')
+    parser.add_argument('--env_name', type=str, default='argparse_env.txt', help='save as env_name.txt')
     args = parser.parse_args()
 
     print(f"args.family={args.family}, args.ffan_name={args.ffan_name}")
+    save_env(args.env_name)
     # parser.print_help()
     # parser.print_usage()
 
--- demo_subprocess.py
+++ demo_subprocess.py
@@ -1,9 +1,35 @@
 import subprocess
+import os
+from argparse_exer import save_env
+
 
 # todo: add demo for subprocess.run()
 def demo_sub_run():
-    pass
+    save_env('subprocess_env.txt')
+    # p1 = subprocess.run(["python3", "argparse_exer.py", "--family", "nvidia"], check=True)
+    p1 = subprocess.run('python3 argparse_exer.py --family ffan', check=True, shell=True)
+    ret = p1.returncode
+    output = p1.stdout
+    args = p1.args
+    print(p1)
+
+
+def demo_sub_popen():
+    os.environ['COMP'] = 'nvidia'
+    env = os.environ
+    # p = subprocess.Popen(['python3', 'argparse_exer.py', '--family', 'fan'], shell=True, env=env)
+    cmd = 'python3 argparse_exer.py --family ffan --env_name sub_popen.txt'
+    p = subprocess.Popen(cmd, shell=True, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    poll = p.poll()
+    out, err = p.communicate()
+    if poll == 0:
+        output = out.decode()
+    else:
+        raise RuntimeError(f'Fail to run {p.args}')
+    # comm = p.communicate()
+    print(p)
 
 
 if __name__ == '__main__':
-    demo_sub_run()
+    # demo_sub_run()
+    demo_sub_popen()
