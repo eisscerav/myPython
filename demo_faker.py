43df961931769ba422f0736c8ddb981fa7227d42
ffan <ffan@nvidia.com>
2022-06-02 14:41:48 +0800

    demo logging

--- demo_faker.py
+++ demo_faker.py
@@ -1,15 +1,21 @@
 from faker import Faker
-
+import logging
 
 # refer to more samples: https://zetcode.com/python/faker/
 # official doc: https://pypi.org/project/Faker/
 def demo_fake():
     fake = Faker()
     name = fake.name()
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug("Harmless debug Message")
+    logging.info("Just an information")
+    logging.warning("Its a Warning")
+    logging.error("Did you try to divide by zero")
+    logging.critical("Internet is down")
     addr = fake.address()
     text = fake.text()
-    age = fake.age()
     print('demo fake done!!')
+
 
 
 if __name__ == '__main__':
