55fa350f5edd7d804ca63c2d16b35ed496b844ff
ffan <ffan@nvidia.com>
2022-06-03 20:44:53 +0800

    update demo for subprocess.Popen

--- demo_subprocess.py
+++ demo_subprocess.py
@@ -17,16 +17,21 @@ def demo_sub_run():
 def demo_sub_popen():
     os.environ['COMP'] = 'nvidia'
     env = os.environ
-    # p = subprocess.Popen(['python3', 'argparse_exer.py', '--family', 'fan'], shell=True, env=env)
-    cmd = 'python3 argparse_exer.py --family ffan --env_name sub_popen.txt'
-    p = subprocess.Popen(cmd, shell=True, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    poll = p.poll()
-    out, err = p.communicate()
-    if poll == 0:
-        output = out.decode()
-    else:
-        raise RuntimeError(f'Fail to run {p.args}')
-    # comm = p.communicate()
+    save_env('subprocess_env.txt')
+    cmd = 'python3 argparse_exer.py --family nvidia --name ampere --env_name sub_popen.txt'
+    cmd = 'ls -l'
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    # p = subprocess.Popen(cmd, shell=True, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # stdout = p.stdout.readlines()  # return a list(looks like separate by newline) and no output later in communicate
+    # stderr = p.stderr.readlines()
+    # Use communicate() rather than .stdin.write, .stdout.read or .stderr.read to avoid deadlock
+    out, err = p.communicate()  # wait for child to finish and return string
+    # output = out.decode()  # no need to decode with text=True
+    if p.returncode:
+        # stderr = err.decode() # no need to decode with text=True
+        raise RuntimeError(f'Fail to run {p.args} with {err}')
+    # p.kill()
+
     print(p)
 
 
