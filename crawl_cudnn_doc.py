6d0c9c503f3ffb4c2094651244d39bc98df59b57
ffan <eisscerav@163.com>
2022-08-17 17:23:51 +0800

    dump output to file

--- crawl_cudnn_doc.py
+++ crawl_cudnn_doc.py
@@ -8,19 +8,30 @@ def parse_cudnn_api(section_number='3.2'):
     response = requests.get(cudnn_doc_url)
     soup = BeautifulSoup(response.text, 'lxml')
     all_h3 = soup.find_all('h3', class_='title topictitle2')
+    pub_api = []
     for h3 in all_h3:
         a = h3.a
         if h3.a.text.startswith(section_number):
             kbd = h3.find('kbd', class_='ph userinput')
             if kbd:
-                print(kbd.text)
-
-    print('done parse_op_infer_api')
+                # print(kbd.text)
+                pub_api.append(kbd.text+'\n')
+    with open('out.txt', 'w') as f:
+        f.writelines(pub_api)
+    print('done parse_cudnn_api')
 
 
 def main():
-    # section 4.2: ops_train, 5.2: cnn_infer, 6.2: cnn_train, 7.2: adv_infer, 8.2: adv_train
-    parse_cudnn_api('4.2')
+    # section 4.1: ops_train, 5.2: cnn_infer, 6.2: cnn_train, 7.2: adv_infer, 8.2: adv_train
+    section = {
+        'ops_infer': '3.2',
+        'ops_train': '4.1',
+        'cnn_infer': '5.2',
+        'cnn_train': '6.2',
+        'adv_infer': '7.2',
+        'adv_train': '8.2',
+    }
+    parse_cudnn_api(section.get('adv_train'))
 
 
 if __name__ == '__main__':
