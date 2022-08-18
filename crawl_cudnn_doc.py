31b32c34da9e573dff7d62e40294c389d8ff12f9
ffan <eisscerav@163.com>
2022-08-18 09:55:33 +0800

    to get title as well

--- crawl_cudnn_doc.py
+++ crawl_cudnn_doc.py
@@ -3,6 +3,15 @@ import requests
 
 cudnn_doc_url = 'https://docs.nvidia.com/deeplearning/cudnn/api/index.html'
 
+section = {
+    'ops_infer': '3.2',
+    'ops_train': '4.1',
+    'cnn_infer': '5.2',
+    'cnn_train': '6.2',
+    'adv_infer': '7.2',
+    'adv_train': '8.2',
+}
+
 
 def parse_cudnn_api(section_number='3.2'):
     response = requests.get(cudnn_doc_url)
@@ -14,24 +23,19 @@ def parse_cudnn_api(section_number='3.2'):
         if h3.a.text.startswith(section_number):
             kbd = h3.find('kbd', class_='ph userinput')
             if kbd:
-                # print(kbd.text)
                 pub_api.append(kbd.text+'\n')
-    with open('out.txt', 'w') as f:
+            else:  # should be title
+                name = h3.a.get('name')
+                pub_api.append(name+'\n')
+    with open(section_number+'_out.txt', 'w') as f:
         f.writelines(pub_api)
-    print('done parse_cudnn_api')
+    print(f'done parse_cudnn_api for {section_number}')
 
 
 def main():
     # section 4.1: ops_train, 5.2: cnn_infer, 6.2: cnn_train, 7.2: adv_infer, 8.2: adv_train
-    section = {
-        'ops_infer': '3.2',
-        'ops_train': '4.1',
-        'cnn_infer': '5.2',
-        'cnn_train': '6.2',
-        'adv_infer': '7.2',
-        'adv_train': '8.2',
-    }
-    parse_cudnn_api(section.get('adv_train'))
+    for k, v in section.items():
+        parse_cudnn_api(v)
 
 
 if __name__ == '__main__':
