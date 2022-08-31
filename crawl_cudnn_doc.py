a8b4fc0154f0b37789758deb66f881b98ff4d50a
ffan <eisscerav@163.com>
2022-08-31 22:00:26 +0800

    update workflow

--- crawl_cudnn_doc.py
+++ crawl_cudnn_doc.py
@@ -13,29 +13,41 @@ section = {
 }
 
 
-def parse_cudnn_api(section_number='3.2'):
-    response = requests.get(cudnn_doc_url)
-    soup = BeautifulSoup(response.text, 'lxml')
+def parse_cudnn_api(soup, section_number='3.2'):
+    # response = requests.get(cudnn_doc_url)
+    # soup = BeautifulSoup(response.text, 'lxml')
     all_h3 = soup.find_all('h3', class_='title topictitle2')
     pub_api = []
     for h3 in all_h3:
         a = h3.a
         if h3.a.text.startswith(section_number):
             kbd = h3.find('kbd', class_='ph userinput')
-            if kbd:
-                pub_api.append(kbd.text+'\n')
-            else:  # should be title
-                name = h3.a.get('name')
-                pub_api.append(name+'\n')
-    with open(section_number+'_out.txt', 'w') as f:
-        f.writelines(pub_api)
-    print(f'done parse_cudnn_api for {section_number}')
+            if kbd: # should be API name
+                api_name = kbd.text.replace('()', '')
+                pub_api.append(api_name)
+                # pub_api.append(api_name+'\n')
+            # else:  # should be title
+            #     name = h3.a.get('name')
+            #     pub_api.append(name+'\n')
+    # with open(section_number+'_out.txt', 'w') as f:
+    #     f.writelines(pub_api)
+    # print(f'done parse_cudnn_api for {section_number}')
+    return pub_api
+
+
+def get_soup():
+    response = requests.get(cudnn_doc_url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
 
 
 def main():
+    soup = get_soup()
+    pub_api = []
     # section 4.1: ops_train, 5.2: cnn_infer, 6.2: cnn_train, 7.2: adv_infer, 8.2: adv_train
     for k, v in section.items():
-        parse_cudnn_api(v)
+        pub_api += parse_cudnn_api(soup, v)
+    print('done ')
 
 
 if __name__ == '__main__':
