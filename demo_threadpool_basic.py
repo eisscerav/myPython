35a2b7a30cdbcb29f97960ff52bca43b3f049727
ffan <ffan@nvidia.com>
2022-10-28 10:59:15 +0800

    demo decorator for threading lock

--- demo_threadpool_basic.py
+++ demo_threadpool_basic.py
@@ -1,17 +1,28 @@
+import time
 from multiprocessing import Process
 from multiprocessing.pool import ThreadPool as Pool
 from faker import Faker
 import random
+import threading
 
+def synchronized(func):
+    func.__lock__ = threading.Lock()
 
-def f(x, y):
+    def synced_func(*args, **kws):
+        with func.__lock__:
+            return func(*args, **kws)
+
+    return synced_func
+
+def f(x, y, z):
     ret = ''
     fake = Faker()
     profile = fake.simple_profile()
-    profile[x] = y
+    profile[x] = y + z
     ret += profile.get('name')
     ret += profile.get('mail')
     ret += profile.get('address')
+    time.sleep(1)
     return profile
 
 def wrapper_f(args):
@@ -20,8 +31,11 @@ def wrapper_f(args):
 if __name__ == '__main__':
     X = random.sample(range(1, 30), 20)
     Y = random.sample(range(1, 100), 20)
+    Z = random.sample(range(100, 1000), 20)
     # start 4 worker processes
     with Pool(processes=4) as pool:
         # print "[0, 1, 4,..., 81]"
-        out = pool.map(wrapper_f, zip(X, Y))
+        out = pool.map(wrapper_f, zip(X, Y, Z))
+        pool.close()
+        pool.join()
         print('done main')
