bc50c14198fbb4667b569d580c4c55b5fe096400
eisscerav <eisscerav@163.com>
2022-12-08 20:01:28 +0800

    update demo json

--- demo_json.py
+++ demo_json.py
@@ -3,9 +3,20 @@ import json
 
 def read_json(file='data/coverage_history_results_A100.json'):
     with open(file, 'r') as fp:
-        data = json.load(fp)
+        json_data = json.load(fp)
+    my_data = {'Abs_abstract': 1, 'fancy': 'nvidia'}
+    json_data.update(my_data)
     print('done read_json')
+    return json_data
+
+
+def write_json(dict_data):
+    with open('new.json', 'w') as f:
+        json.dump(dict_data, f, indent=4)
+    print('done dump_to_file')
 
 
 if __name__ == '__main__':
-    read_json()
+    json_file = 'data/fusionGraphTests.json'
+    data = read_json(json_file)
+    write_json(data)
