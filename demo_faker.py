e69075f2f2473f4ca37a624c9e25ddb32ce6040e
eisscerav <eisscerav@163.com>
2022-12-14 11:27:32 +0800

    update demo fake

--- demo_faker.py
+++ demo_faker.py
@@ -1,13 +1,17 @@
 from faker import Faker
 import logging
+import datetime
 
 # refer to more samples: https://zetcode.com/python/faker/
 # official doc: https://pypi.org/project/Faker/
 def demo_fake():
+    current_year = datetime.datetime.now().year
     fake = Faker()
     profile1 = fake.simple_profile()
     profile2 = fake.profile()
     name = fake.name()
+    year = profile1.get('birthdate').year
+    age = current_year - year
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logging.debug("Harmless debug Message")
     logging.info("Just an information")
