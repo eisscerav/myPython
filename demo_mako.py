81e04448929f0302359b9e482eec0a6b15f78273
eisscerav <eisscerav@163.com>
2024-03-07 15:46:30 +0800

    generate report with mako

--- demo_mako.py
+++ demo_mako.py
@@ -1,19 +1,24 @@
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
+
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, link):
         self.name = name
         self.age = age
+        self.link = link
+
 
 def demo_mako_template():
-    t = Template(filename='data/demo.tmpl')
+    t = Template(filename='data/demo.mako')
     my_dict = {
         'company': 'nvidia',
         'value': 300
     }
     # r1 = t.render(name='mako', x=5, y=2, my_company=my_dict)
-    person = Person('fancy', 40)
+    p1 = Person('fancy', 40, "http://fancy.com")
+    p2 = Person('dili', 39, "http://dili/com")
+    persons = [p1, p2]
     mylist = [
         # {'comp': 'nv', 'val': 200},
         # {'comp': 'google', 'val': 400}
@@ -23,11 +28,14 @@ def demo_mako_template():
         'x': 5,
         'y': 2,
         'my_company': my_dict,
-        'person': person,
+        'person': p1,
+        # 'persons': persons,
         # 'mylist': ['fancy', 'ffan', 'ffan_local']
         'mylist': mylist
     }
     r2 = t.render(**data)
+    with open("demo_mako.html", "w") as fp:
+        fp.write(r2)
     print(r2)
 
     # refer to https://docs.makotemplates.org/en/latest/usage.html#using-templatelookup
@@ -37,5 +45,29 @@ def demo_mako_template():
     # print(r2)
 
 
+def demo_generate_report():
+    t = Template(filename='data/report.mako')
+    td_items = []
+    td_item= {'ID': 1, 'File': "file1.txt", 'file_href': "www.abc.com"}
+    td_items.append(td_item)
+    td_item['ID'] = 2
+    td_item['File'] = "file2.txt"
+    td_item['file_href'] = "www.xyc.com"
+    td_items.append(td_item)
+
+    data = {
+        'project_name': 'demo_project',
+        'td_items': td_items,
+        'original_link': "www.old.com"
+    }
+
+    r2 = t.render(**data)
+    with open("demo_report.html", "w") as fp:
+        fp.write(r2)
+
+    print('done')
+    return
+
+
 if __name__ == '__main__':
-    demo_mako_template()
+    demo_generate_report()
